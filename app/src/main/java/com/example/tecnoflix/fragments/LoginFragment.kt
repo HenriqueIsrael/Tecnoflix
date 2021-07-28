50e98c6e67effca70195a3a6efad8e01d5046fe8
HenriqueIsrael <henriqueisraeloliveira74@gmail.com>
2021-07-28 20:46:56 -0300

    Inicio da implementação da arquitetura MVVM.

--- app/src/main/java/com/example/tecnoflix/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/tecnoflix/fragments/LoginFragment.kt
@@ -5,14 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.tecnoflix.R
+import com.example.tecnoflix.ViewModelFactory
 import com.example.tecnoflix.databinding.LoginBinding
+import com.example.tecnoflix.viewmodel.LoginViewModel
 
-class LoginFragment: Fragment() {
+class LoginFragment : Fragment() {
 
     private var _binding: LoginBinding? = null
     private val binding: LoginBinding get() = _binding!!
+
+    private lateinit var viewModel: LoginViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +30,32 @@ class LoginFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelFactory(requireContext())
+        ).get(LoginViewModel::class.java)
+
+        viewModel.getEmail()
+
+        viewModel.emailLiveData.observe(viewLifecycleOwner, {
+            binding.campoEmail.setText(it)
+        })
+
+        viewModel.getSenha()
+
+        viewModel.senhaLiveData.observe(viewLifecycleOwner, {
+            binding.campoSenha.setText(it)
+        })
+
+        binding.switchSalvarLogin.setOnCheckedChangeListener { _ , isChecked ->
+            if(isChecked){
+                viewModel.saveLogin(binding.campoEmail.text.toString(), binding.campoSenha.text.toString())
+            } else {
+                viewModel.saveLogin("","")
+            }
+        }
+
         binding.btEntrar.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_homeActivity)
         }
--- app/src/main/java/com/example/tecnoflix/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/tecnoflix/viewmodel/LoginViewModel.kt
@@ -1,19 +1,27 @@
 package com.example.tecnoflix.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.tecnoflix.repository.LoginRepository
 
 class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel() {
+
+    private val _emailLiveData = MutableLiveData <String>()
+    val emailLiveData: LiveData <String> = _emailLiveData
+
+    private val _senhaLiveData = MutableLiveData <String>()
+    val senhaLiveData: LiveData <String> = _senhaLiveData
 
     fun saveLogin(email: String, senha: String) {
         loginRepository.saveLogin(email, senha)
     }
 
     fun getEmail() {
-
+        _emailLiveData.postValue(loginRepository.getEmail())
     }
 
     fun getSenha() {
-
+        _senhaLiveData.postValue(loginRepository.getSenha())
     }
 }
